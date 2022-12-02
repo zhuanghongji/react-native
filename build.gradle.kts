7401d2a620f153a24037dfd1ae5bf85a6e0d32e9
Nicola Corti <ncor@meta.com>
2022-12-02 19:04:52 +0100

    Realing to 7.4.0-beta05

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ buildscript {
     gradlePluginPortal()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.4.0-rc01")
+    classpath("com.android.tools.build:gradle:7.4.0-beta05")
     classpath("de.undercouch:gradle-download-task:5.0.1")
   }
 }
